a892c083c7e765f3ac049693b9d1dda829a7b10e
Jorge Roncel <jorgeron1993@hotmail.com>
2016-02-24 16:26:10 +0100

    Configuración de idiomas
    Creado el idioma español y eliminado el francés. La aplicación correrá
    en español y en inglés.

--- TFGProject/src/main/webapp/scripts/components/language/language.service.js
+++ TFGProject/src/main/webapp/scripts/components/language/language.service.js
@@ -27,7 +27,8 @@ angular.module('tFGProjectApp')
  They are written in English to avoid character encoding issues (not a perfect solution)
  */
     .constant('LANGUAGES', [
-        'en', 'fr'
+        'en'
+        ,'es'
         //JHipster will add new languages here
     ]
 );
